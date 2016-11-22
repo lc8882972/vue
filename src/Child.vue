dbff705804378614dc714fd41c6c64c3326c2254
lc8882972 <guoyan2972@qq.com>
2016-11-22 23:34:06 +0800

    添加父子组件

---
+++ src/Child.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props:["name"]
+}
+</script>
+<style>
+   p{
+     margin:0;
+   }  
+</style>
---
+++ src/Parent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>{{msg}}</p>
+        <abc name="abc"><abc>
+    </div>
+</template>
+
+<script>
+import child from './Child.vue'
+export default {
+  data(){
+    return {
+      msg:'爸爸组件'
+    }
+  },
+   components: {
+      abc:child
+   }
+}
+</script>
+
+<style>
+
+</style>
